afa9b2cde77b6930dc77edcd071521b71467ea07
maheshladke <maheshladke1610@gmail.com>
2026-01-17 13:12:51 +0530

    Updated EC2 instance configuration

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
+
+variable "key_name" {
+  description = "SSH key pair name (must exist in AWS)"
+  type        = string
+  default = "Terraform_project1"
+}
+
