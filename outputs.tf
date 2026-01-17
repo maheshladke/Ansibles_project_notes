5f43c7a618d91ab3d3d6caf6497fdc9f911312fb
maheshladke <98413869+maheshladke@users.noreply.github.com>
2026-01-17 13:16:52 +0530

    Delete outputs.tf

--- outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.web.public_ip
-}
-
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.web.id
-}
